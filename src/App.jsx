fd37efbed6ccecc83e2d4bc39c37302f3e696ba2
Nathan Agbayani <nathan.pagbayani@gmail.com>
2024-02-02 18:13:58 -0800

    React Tiles Boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
-export function App(props) {
+const style = {
+  letter: {
+    // Add some basic styling to the button
+    padding: "5px 10px",
+    margin: "5px",
+    fontSize: "16px",
+    fontWeight: "bold",
+    border: "1px solid black",
+    borderRadius: "5px",
+    backgroundColor: "lightgray",
+    cursor: "pointer",
+  },
+};
+function Tile({ letter, updateOutput, clickedLetters }) {
+  const handleClick = () => {
+    updateOutput(letter);
+  };
+
   return (
-    <div className='App'>
-      <h1>Hello React.</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <button style={style.letter} onClick={handleClick}>
+      {letter}
+    </button>
   );
 }
 
-// Log to console
-console.log("Hello console");
+export function App() {
+  const [output, setOutput] = useState("");
+  const [clickedLetters, setClickedLetters] = useState([]);
+  const [counter, setCounter] = useState(0); // Manage the counter state
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+  const handleTileClick = (letter) => {
+    const lastClickedLetter = clickedLetters[clickedLetters.length - 1];
+
+    if (lastClickedLetter !== letter) {
+      // If letter is different, add it to output and reset counter
+      setClickedLetters([...clickedLetters, letter]);
+      setOutput(output + letter);
+      setCounter(0); // Reset counter
+    } else if (lastClickedLetter === letter && counter < 2) {
+      // If letter is the same but counter is less than 2, add it to output and increment counter
+      setClickedLetters([...clickedLetters, letter]);
+      setOutput(output + letter);
+      setCounter(counter + 1); // Increment counter
+    } else if (lastClickedLetter === letter && counter === 2) {
+      // If letter is the same and counter is 2, replace it with underscore and reset counter
+      setOutput(output.substring(0, output.length - 3) + "_");
+      setCounter(0); // Reset counter
+    }
+  };
+
+  return (
+    <div>
+      <div>Output: {output}</div>
+      {alphabet.map((letter, index) => (
+        <Tile
+          key={index}
+          letter={letter}
+          updateOutput={handleTileClick}
+          clickedLetters={clickedLetters}
+        />
+      ))}
+    </div>
+  );
+}
