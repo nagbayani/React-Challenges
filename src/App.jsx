d2766c1755f90ffe479e180d19f47bd56a328242
Nathan Agbayani <nathan.pagbayani@gmail.com>
2024-02-12 12:40:55 -0800

    User

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 
+/*
+ * Implement the user registration form UI
+ * Validate the form inputs and display error messages
+ * Integrate form submission with a mock API call (simulated delay).
+ * Add a success message upon successful registration
+ */
 export function App(props) {
+  const validateForm = () => {};
+
   return (
     <div className='App'>
-      <h1>Hello React.</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className='submission-form'>
+        <form>
+          <label htmlFor='name'>
+            Name
+            <input type='text' id='name' name='name' placeholder='Your name' />
+          </label>
+          <label htmlFor='email'>
+            Email
+            <input
+              type='text'
+              id='email'
+              name='email'
+              placeholder='Your Email'
+            />
+          </label>
+          <label htmlFor='phoneNumber'>
+            Phone Number
+            <input
+              type='text'
+              id='phoneNumber'
+              name='phoneNumber'
+              placeholder='Your Phone Number'
+            />
+          </label>
+        </form>
+        {/* Input Field=> name, type, placeholder, label */}
+      </div>
     </div>
   );
 }
